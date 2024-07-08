af6e4c50e00658c127ad83d34faed098d16b6e33
polina-v80 <166666650+polina-v80@users.noreply.github.com>
2024-07-08 15:46:25 +0300

    Add files via upload
    Задача "Счётчик вызовов"

---
+++ module_3_1.py
@@ -0,0 +1,24 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string = string.lower()
+    list_to_search = [item.lower() for item in list_to_search]
+    if string in list_to_search:
+        return True
+    else:
+        return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
